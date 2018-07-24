b588fefe23a5edfc50b181f6e30553ba2b798b0b
Christian Timberlake II <timberlakece1@my.palmbeachstate.edu>
2018-07-24 01:10:51 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -26,4 +26,8 @@ class CashRegister
       "There is no discount to apply."
     end
   end
+
+  def void_last_transaction
+    self.void_last_transaction = self.total - self.last_transaction
+  end
 end
